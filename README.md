ce3129e71054a0ee3d2d030d718201c3498df170
Julien Prugne <julien.prugne@gmail.com>
2016-05-09 15:30:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Fake endpoint
+
+Fake api endpoint, accept connection on port 5000.
+Log requests
---
+++ index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const colors = require("colors");
+
+let app = express();
+
+app.use(bodyParser());
+
+app.get("*", (req, res) => {
+  console.log(`[GET] `.green);
+  console.log(`Body: ${JSON.stringify(req.body)}`.green);
+  console.log(`Params: ${JSON.stringify(req.params)}`.green);
+  console.log(`Query: ${JSON.stringify(req.query)}`.green);
+  res.status(200).send("ok");
+});
+
+app.post("*", (req, res) => {
+  console.log(`[POST]`.blue);
+  console.log(`Body: ${JSON.stringify(req.body)}`.blue);
+  console.log(`Params: ${JSON.stringify(req.params)}`.blue);
+  console.log(`Query: ${JSON.stringify(req.query)}`.blue);
+  res.status(200).send("ok");
+});
+
+app.put("*", (req, res) => {
+  console.log(`[PUT]`.yellow);
+  console.log(`Body: ${JSON.stringify(req.body)}`.yellow);
+  console.log(`Params: ${JSON.stringify(req.params)}`.yellow);
+  console.log(`Query: ${JSON.stringify(req.query)}`.yellow);
+  res.status(200).send("ok");
+});
+
+app.delete("*", (req, res) => {
+  console.log(`[DELETE]`.red);
+  console.log(`Body: ${JSON.stringify(req.body)}`.red);
+  console.log(`Params: ${JSON.stringify(req.params)}`.red);
+  console.log(`Query: ${JSON.stringify(req.query)}`.red);
+  res.status(200).send("ok");
+});
+
+app.listen(5000);
